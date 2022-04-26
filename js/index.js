f2ab454722dcf4120ff66d351935ab0873232420
osamamishmish <osama.mishmish16@gmail.com>
2022-04-26 04:38:57 +0200

    add accounts to home page

--- js/index.js
+++ js/index.js
@@ -6,25 +6,21 @@ $(".nav-link").on("mouseleave",function(){
 });
 
 
-
-        $(".description").children().hide();
-        $(".description").children().eq(0).show();
-        $(".description").children().eq(0).delay(2000).hide(3000);
-        $(".description").children().eq(1).delay(3000).show(2000);
-        $(".description").children().eq(1).delay(3000).hide(2000);
-        $(".description").children().eq(2).delay(5000).show(2000);
-        $(".description").children().eq(2).delay(3000).hide(2000);
-        $(".description").children().eq(3).delay(7000).show(2000);
-        $(".description").children().eq(3).delay(3000).hide(2000);
-        $(".description").children().eq(4).delay(9000).show(2000);
-        $(".description").children().eq(4).delay(3000).hide(2000);
-        $(".description").children().eq(5).delay(10000).show(2000);
-        $(".description").children().eq(5).delay(4000).hide(2000);
-        $(".description").children().eq(6).delay(12000).show(2000);
-        $(".description").children().eq(6).delay(3000).hide(2000);
-        $(".description").children().eq(7).delay(15000).show(2000);
-        $(".description").children().eq(8).delay(15000).show(2000);
-        
+$(window).on("load",function(){
+    $(".description").children().hide();
+    $(".description").children().eq(0).show().delay(2000).hide(3000);
+    $(".description").children().eq(1).delay(3000).show(2000).delay(3000).hide(2000);
+    $(".description").children().eq(2).delay(5000).show(2000).delay(3000).hide(2000);
+    $(".description").children().eq(3).delay(7000).show(2000).delay(3000).hide(2000);
+    $(".description").children().eq(4).delay(9000).show(2000).delay(3000).hide(2000);
+    $(".description").children().eq(5).delay(13000).show(2000).delay(4000).hide(2000);
+    $(".description").children().eq(6).delay(15000).show(2000).delay(3000).hide(2000);
+    $(".description").children().eq(7).delay(18000).show(2000);
+    $(".description").children().eq(8).delay(18000).show(2000);
+    $(".description").children().eq(9).delay(18000).show(2000);
+    
+})
+       
 //$(".description").children().eq(1).hide(9000);
     
     
