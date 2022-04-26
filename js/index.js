730cc836954d7aee7a54d06ed2eda94ee70db800
osamamishmish <osama.mishmish16@gmail.com>
2022-04-26 02:53:20 +0200

    add introduction view

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(".nav-link").on("mouseenter",function(){
+    $(this).addClass("hovered");
+});
+$(".nav-link").on("mouseleave",function(){
+    $(this).removeClass("hovered");
+});
+
+
+
+        $(".description").children().hide();
+        $(".description").children().eq(0).show();
+        $(".description").children().eq(0).delay(2000).hide(3000);
+        $(".description").children().eq(1).delay(3000).show(2000);
+        $(".description").children().eq(1).delay(3000).hide(2000);
+        $(".description").children().eq(2).delay(5000).show(2000);
+        $(".description").children().eq(2).delay(3000).hide(2000);
+        $(".description").children().eq(3).delay(7000).show(2000);
+        $(".description").children().eq(3).delay(3000).hide(2000);
+        $(".description").children().eq(4).delay(9000).show(2000);
+        $(".description").children().eq(4).delay(3000).hide(2000);
+        $(".description").children().eq(5).delay(10000).show(2000);
+        $(".description").children().eq(5).delay(4000).hide(2000);
+        $(".description").children().eq(6).delay(12000).show(2000);
+        $(".description").children().eq(6).delay(3000).hide(2000);
+        $(".description").children().eq(7).delay(15000).show(2000);
+        $(".description").children().eq(8).delay(15000).show(2000);
+        
+//$(".description").children().eq(1).hide(9000);
+    
+    
